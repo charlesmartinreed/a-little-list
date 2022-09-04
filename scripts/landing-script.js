c95d363dba04cdda9d168bb4190e130e7871a878
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-09-03 23:59:08 -0500

    Change landing page animation.

--- scripts/landing-script.js
+++ scripts/landing-script.js
@@ -1,162 +1,73 @@
-let holdingPen = document.querySelector(".holding-pen");
-
-let emojiContainers;
+let iconDiv = document.querySelector(".emoji-icon");
+let textDiv = document.querySelector(".emoji-text");
+let emojiDiv = document.querySelector(".emoji-carousel");
+let pageContainer = document.querySelector(".container-landing-page");
 
 let emojiIcons = [
-  "🍕",
-  "🍊",
-  "🍌",
-  "🍑",
-  "🍍",
-  "🍞",
-  "🍒",
-  "🍓",
-  "🥝",
-  "🥚",
-  "🍉",
-  "🍅",
-  "🍤",
-  "🥕",
-  "🍰",
-  "🍩",
-  "🫒",
-  "🥐",
-  "🍜",
-  "🍚",
+  ["🍕", "Pizza"],
+  ["🍊", "Oranges"],
+  ["🍌", "Bananas"],
+  ["🍑", "Peaches"],
+  ["🍍", "Pineapples"],
+  ["🍞", "Bread"],
+  ["🍒", "Cherries"],
+  ["🍓", "Strawberries"],
+  ["🥝", "Kiwis"],
+  ["🍗", "Chicken"],
+  ["🥑", "Avocados"],
+  ["🥚", "Eggs"],
+  ["🍉", "Watermelon"],
+  ["🍅", "Tomatoes"],
+  ["🥛", "Milk"],
+  ["🍤", "Shrimp"],
+  ["🥕", "Carrots"],
+  ["🥧", "Pie"],
+  ["🍩", "Donuts"],
+  ["🫒", "Olives"],
+  ["🍎", "Apples"],
+  ["🥐", "Croissants"],
+  ["🥩", "Steak"],
 ];
-let emojiPositions = [];
 
-let pageHeight = window.innerHeight;
-let pageWidth = window.innerWidth;
-
-let emojiCount = 75;
-
-function initContainers() {
-  emojiContainers = [];
-
-  for (let i = 0; i < emojiCount; i++) {
-    let div = document.createElement("div");
-    div.classList.add("bg-emoji-container");
-    holdingPen.appendChild(div);
-    // emojiContainers.push(div);
+function renderEmojiContainer() {
+  let [icon, text] = returnRandomEmojiArr();
+  let currentIcon = icon.innerText;
+  if (icon === currentIcon) {
+    [icon, text] = renderEmojiContainer();
   }
 
-  emojiContainers = document.querySelectorAll(".bg-emoji-container");
+  emojiDiv.remove();
+
+  emojiDiv.removeChild(iconDiv);
+  emojiDiv.removeChild(textDiv);
+
+  iconDiv.innerText = icon;
+  textDiv.innerText = text;
+
+  pageContainer.insertBefore(
+    emojiDiv,
+    document.querySelector("#cta-landing-page")
+  );
+  emojiDiv.appendChild(iconDiv);
+  emojiDiv.appendChild(textDiv);
+
+  // emojiDiv.childNodes.forEach((node) => node.remove());
+  // emojiDiv.append(icon, text);
+  // console.log(emojiDiv.childNodes);
+  returnRandomEmojiArr();
 }
 
-function initEmojis() {
-  emojiContainers.forEach((container) => {
-    // let delayAmt = emoji.getAttribute("data-animation-delay");
-    // emoji.style.animationDelay = `${delayAmt}`;
-    let emoji = emojiIcons[Math.floor(Math.random() * emojiIcons.length)];
-    // console.log(emoji);
-    setEmojiPosition(container, emoji);
-    container.style.animationDelay = setAnimationDelay(2);
-    // console.log(emoji.style.top, emoji.style.left);
-  });
+function returnRandomEmojiArr() {
+  let emoji = emojiIcons[Math.round(Math.random() * emojiIcons.length - 1)];
+  return emoji;
 }
-
-function setAnimationDelay(totalSeconds) {
-  return `${Math.random() * totalSeconds}s`;
-}
-
-function setEmojiPosition(container, emoji) {
-  container.innerText = emoji;
-
-  console.log(pageWidth, pageHeight);
-
-  let proposedXPos;
-  let proposedYPos;
-
-  if (pageWidth <= 600) {
-    proposedXPos = Math.round(Math.random() * pageWidth * 0.75);
-  } else {
-    proposedXPos = Math.round(Math.random() * pageWidth);
-  }
-
-  if (pageHeight <= 600) {
-    proposedYPos = Math.round(Math.random() * pageHeight * 0.75);
-  } else {
-    proposedYPos = Math.round(Math.random() * pageHeight);
-  }
-
-  let distanceModifier = 48;
-
-  if (emojiPositions.length > 0) {
-    emojiPositions.forEach((emoji) => {
-      let x = emoji.position[0];
-      let y = emoji.position[1];
-
-      //   if (Math.abs(proposedXPos - x) <= 36) {
-      //     let difference = Math.abs(proposedXPos - x);
-      //     proposedXPos = x + difference;
-      //   }
-
-      //   if (Math.abs(proposedYPos - y) <= 36) {
-      //     let difference = Math.abs(proposedYPos - y);
-      //     proposedYPos = y + difference;
-      //   }
-    });
-  }
-
-  //   if (proposedXPos > pageWidth) {
-  //     let difference = Math.abs(proposedXPos - pageWidth);
-  //     proposedXPos = pageWidth - difference;
-  //     console.log("difference", difference, "new x", proposedXPos);
-  //   }
-
-  //   if (proposedYPos > pageHeight) {
-  //     let difference = Math.abs(proposedYPos - pageHeight);
-  //     proposedYPos = pageWidth - difference;
-  //     console.log("difference", difference, "new x", proposedXPos);
-  //   }
-
-  //   if (proposedYPos >= innerHeight + 48) {
-  //     proposedYPos = proposedYPos / 2 - 96;
-  //   }
-
-  emojiPositions.push({
-    emoji: emoji,
-    position: [
-      proposedXPos + distanceModifier,
-      proposedYPos + distanceModifier,
-    ],
-  });
-
-  console.log(proposedXPos, proposedYPos);
-
-  container.style.top = `${proposedYPos}px`;
-  container.style.left = `${proposedXPos}px`;
-}
-
-function calcTotalEmojis() {
-  // assume a size of 72px
-  // need space of at 36px on each side
-  // number of rows = pageWidth
-  // 500
-  //   let startingXCoord = 36;
-  //   let startingYCoord = 36;
-  //   let emojiCoords = [];
-}
-
-window.addEventListener("resize", () => {
-  pageHeight = window.innerHeight;
-  pageWidth = window.innerWidth;
-
-  //   while (startingXCoord < pageWidth) {
-  //     while (startingYCoord < pageHeight) {
-  //     }
-  //   }
-
-  initEmojis();
-
-  //   console.log(pageHeight, pageWidth);
-});
 
 window.addEventListener("DOMContentLoaded", () => {
-  //   console.log("pageheight/pagewidth", pageHeight, pageWidth);
-
-  calcTotalEmojis();
-  initContainers();
-  initEmojis();
+  setInterval(
+    (function startInterval() {
+      renderEmojiContainer();
+      return startInterval;
+    })(),
+    3500
+  );
 });
