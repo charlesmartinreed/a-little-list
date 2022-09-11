551a5034c60570a7d5403f4bbcfdfc2f3f8e3640
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-09-11 16:48:07 -0500

    Add 404 page redirect.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { JSDOM } = require("jsdom");
 
 const path = require("path");
 const express = require("express");
+const { rmSync } = require("fs");
 
 const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.SERVER_PORT || 6500;
@@ -67,6 +68,10 @@ app.get("/list", (req, res) => {
   }
 
   res.sendFile(`${__dirname}/list.html`);
+});
+
+app.use((req, res, next) => {
+  res.status(404).redirect("/");
 });
 
 // app.use(express.static(path.join(__dirname, "public/css")));
